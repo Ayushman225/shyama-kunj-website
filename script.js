03feff031f15ca0663f75c97ece9bb4149dc2ff3
Ayushman225 <ayushmanp632@gmail.com>
2026-04-01 12:21:25 +0530

    Create script.js with JavaScript functionality to display current date and time in UTC.

---
+++ script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Function to display current date and time in UTC
+function displayCurrentDateTime() {
+    const now = new Date();
+    const utcDateTime = now.toISOString().replace('T', ' ').substring(0, 19);
+    console.log(`Current Date and Time (UTC): ${utcDateTime}`);
+}
+
+displayCurrentDateTime();
